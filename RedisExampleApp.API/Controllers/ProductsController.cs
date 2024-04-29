ba9404d8fe72a86d02bb2fe28a38fa079f85eacc
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-04-29 19:03:51 +0300

    .

---
+++ RedisExampleApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisExampleApp.API.Repositories.Interfaces;
+
+namespace RedisExampleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productRepository.GetAsync());
+        }
+
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await _productRepository.GetByIdAsync(id));
+        }
+    }
+}
---
+++ RedisExampleApp.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using RedisExampleApp.API.Models;
+
+namespace RedisExampleApp.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAsync();
+        Task<Product> GetByIdAsync(Guid Id);
+        Task<Product> CreateAsync(Product product);
+    }
+}
